91f6ce1870cd934cc64be67921c244ceb70c974f
P-Hemal <abdulhakim.hemal.99@gmail.com>
2024-03-09 08:22:52 +0600

    sum of numbers

--- findOddSum.js
+++ findOddSum.js
@@ -1,23 +1,23 @@
-function getSumOfAnArray(numbers){
+function getSumOfAnArray(numbers) {
     let sum = 0;
-    for(let i = 0; i < numbers.length; i++){
+    for (let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index];
         sum = sum + element;
         // console.log(index, element, sum);
-        
+
     }
     return sum;
-} 
+}
 // const myNumbers =[12, 23, 37, 44, 52, 71, 85, 98];
 // getSumOfAnArray(myNumbers);
 
-function getOddNumbersOfAnArray(numbers){
+function getOddNumbersOfAnArray(numbers) {
     const oddNumbers = [];
-    for(let i = 0; i < numbers.length; i++){
+    for (let i = 0; i < numbers.length; i++) {
         const index = i;
         const element = numbers[index];
-        if(element % 2 === 1){
+        if (element % 2 === 1) {
             console.log(index, element);
             oddNumbers.push(element);
         }
@@ -26,7 +26,7 @@ function getOddNumbersOfAnArray(numbers){
 }
 
 
-const myNumbers =[12, 23, 37, 44, 52, 71, 85, 98];
+const myNumbers = [12, 23, 37, 44, 52, 71, 85, 98];
 const oddNumbers = getOddNumbersOfAnArray(myNumbers);
 console.log(oddNumbers);
 const oddNumberSum = getSumOfAnArray(oddNumbers);
